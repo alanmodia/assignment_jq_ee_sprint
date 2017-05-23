dcff96a996d86140ebcdc1f169cddf15f8c97356
alanmodia <github@alanmillar.com>
2017-05-22 18:41:25 -0700

    in office hours

--- myJS.js
+++ myJS.js
@@ -1,22 +1,56 @@
+// $(document).ready(function() {
 
 
 
-const inputs = document.querySelectorAll('input');
-const textareas = document.querySelectorAll('textarea');
+// input for textarea
+$('textarea').keyup(updateCount);
+$('textarea').keydown(updateCount);
 
-function logText(e) {
-	console.log(this);
-	e.stopPropagation();
-	// console.log(this);
+function updateCount() {
+var cha = $(this).val().length;
+$('#display-count-about').text(cha);
 }
 
-// document.body.addEventListener('click', logText);
+// input for class enter-first-name
+$('input.enter-first-name').keyup(updateCountFirstName);
+$('input.enter-first-name').keydown(updateCountFirstName);
 
-inputs.forEach(input => input.addEventListener('click', logText, {
-	capture:false
-}));
+function updateCountFirstName() {
 
-textareas.forEach(textarea => textarea.addEventListener('click', logText, {
-	capture:false
-}));
+var cha = $(this).val().length;
+if ( cha > 0 ) {
+	$('#display-count-first-name').removeClass( 'counter');
+	// $('#display-count-first-name').show();
+	// $('#display-count-first-name').toggleClass( 'counter');
+	$('#display-count-first-name').text(cha);
+	} else {
 
+	$('#display-count-first-name').addClass( 'counter');
+	//$('#display-count-first-name').hide();
+	}
+}
+
+// input for class enter-pw
+$('input.enter-pw').keyup(updateCountPw);
+$('input.enter-pw').keydown(updateCountPw);
+
+function updateCountPw() {
+var cha = $(this).val().length;
+$('#display-count-pw').text(cha);
+}
+
+// input for class confirm-pw
+$('input.confirm-pw').keyup(updateCountConfirmPw);
+$('input.confirm-pw').keydown(updateCountConfirmPw);
+
+function updateCountConfirmPw() {
+var cha = $(this).val().length;
+$('#display-count-confirm-pw').text(cha);
+}
+
+
+///////////////////////////////
+
+
+
+// });
