db3fa774243e3b1fe77e19a03914f8859dd01188
alanmodia <github@alanmillar.com>
2017-06-05 10:36:05 -0700

    Completed first refactor. Next up, event delegation.

--- myJS.js
+++ myJS.js
@@ -1,56 +1,80 @@
 // $(document).ready(function() {
 
-
-
-// input for textarea
-$('textarea').keyup(updateCount);
-$('textarea').keydown(updateCount);
-
-function updateCount() {
-var cha = $(this).val().length;
-$('#display-count-about').text(cha);
-}
-
-// input for class enter-first-name
-$('input.enter-first-name').keyup(updateCountFirstName);
-$('input.enter-first-name').keydown(updateCountFirstName);
-
-function updateCountFirstName() {
-
-var cha = $(this).val().length;
-if ( cha > 0 ) {
-	$('#display-count-first-name').removeClass( 'counter');
-	// $('#display-count-first-name').show();
-	// $('#display-count-first-name').toggleClass( 'counter');
-	$('#display-count-first-name').text(cha);
+//// addRemove class
+var addRemove = function(cha, $target) {
+if (cha > 0 ) {
+	$target.removeClass( 'counter');
+	$target.text(cha);
+	// alert("addRemove is running");
 	} else {
-
-	$('#display-count-first-name').addClass( 'counter');
-	//$('#display-count-first-name').hide();
+	$target.addClass( 'counter');
 	}
+};
+
+// NOTE: 
+// first-name will now run like confirm-pw, but only if I comment out confirm-pw. This is because each instance is setting the var $elem? So I made each var unique. They all run fine now.
+
+// ===== first-name =====
+
+var $elemFn = $('input.enter-first-name');
+$elemFn.on('keyup keydown', function() {
+	updateCountFirstName($elemFn);
+});
+
+function updateCountFirstName($elemFn) {
+var cha = $elemFn.val().length;
+var $target = $('#display-count-first-name');
+addRemove(cha, $target );
 }
 
-// input for class enter-pw
-$('input.enter-pw').keyup(updateCountPw);
-$('input.enter-pw').keydown(updateCountPw);
+// before refactor
+// $('input.enter-first-name').keyup(updateCountFirstName);
+// $('input.enter-first-name').keydown(updateCountFirstName);
+// function updateCountFirstName() {
+// var cha = $(this).val().length;
+// if ( cha > 0 ) {
+// 	$('#display-count-first-name').removeClass( 'counter');
+// 	$('#display-count-first-name').text(cha);
+// 	} else {
+// 	$('#display-count-first-name').addClass( 'counter');
+// 	}
+// }
+
+// ===== confirm-pw =====
+var $elemCpw = $('input.confirm-pw');
+$elemCpw.on('keyup keydown', function() {
+	updateCountConfirmPw($elemCpw);
+});
+
+function updateCountConfirmPw($elemCpw) {
+var cha = $elemCpw.val().length;
+var $target = $('#display-count-confirm-pw');
+addRemove(cha, $target );
+}
+
+// ===== enter-pw =====
+var $elemPw = $('input.enter-pw');
+$elemPw.on('keyup keydown', function() {
+	updateCountPw($elemPw);
+});
 
 function updateCountPw() {
-var cha = $(this).val().length;
-$('#display-count-pw').text(cha);
+var cha = $elemPw.val().length;
+var $target = $('display-count-pw');
+addRemove(cha, $target );
 }
 
-// input for class confirm-pw
-$('input.confirm-pw').keyup(updateCountConfirmPw);
-$('input.confirm-pw').keydown(updateCountConfirmPw);
+// ===== textarea =====
+var $elemTextArea = $('text-area');
+$elemTextArea.on('keyup, keydown', function() {
+	updateCountTexTArea($elemTextArea);
+});
 
-function updateCountConfirmPw() {
-var cha = $(this).val().length;
-$('#display-count-confirm-pw').text(cha);
+function updateCountTextArea() {
+	var cha = $elemTextArea.val().length;
+	var $target = $('display-count-about');
+	addRemove(cha, $target );
 }
-
-
-///////////////////////////////
-
 
 
 // });
