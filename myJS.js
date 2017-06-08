14cf9343fe0480b04e97c53abeb3c8524e7bae95
alanmodia <github@alanmillar.com>
2017-06-08 10:42:40 -0700

    refactoring save

--- myJS.js
+++ myJS.js
@@ -2,64 +2,65 @@
 
 $( 'form' ).on('keyup keydown', function( event ) {
 	var $obj = $(event.target);
-	var $display = $('#' + $obj.next().attr('id'));
-	var cha = $obj.val().length;
+	var $display = $obj.next();
+	// console.log($display);
+	var characters = $obj.val().length;
 	// to get the maxlength for this control from function maxLength	
 	// I only need this the first time a new field is triggered, as long as the user has not left this field
-	var $control = $('#' + $obj.attr('id'));
-	console.log($control); 
-	updateCountAndDisplay($obj, $display, cha);
+	var min = $obj.data('min');
+	alert(min);
+	updateCountAndDisplay($obj, $display, characters);
 });
 
 //// updateCountAndDisplay class and update counter
-var updateCountAndDisplay = function($obj, $display, cha) {
-if (cha > 0 ) {
+var updateCountAndDisplay = function($obj, $display, characters) {
+if (characters > 0 ) {
 	// was 	$('#display-count-first-name').removeClass( 'counter');
 	$display.removeClass( 'counter' );
-	$display.text(cha);
+	$display.text(characters);
 	} else {
 	$display.addClass( 'counter' );
 	}
 };
 
 // check the maxLength for this control
-function maxLength($control){
+// function maxLength($control){
 
-	// console.log($control + ' before ');
-   var output = 
-   $control == '#first-name' ? 32
-   : 
-   $control == '#about-yourself' ? 140
-   : 
-   $control == '#password' ? 16
-   : 
-   $control == '#password-confirm' ? 16
-   :
-   0;
-   // console.log(output);
-   return output;
-};
+// 	// console.log($control + ' before ');
+//    var output = 
+//    $control == '#first-name' ? 32
+//    : 
+//    $control == '#about-yourself' ? 140
+//    : 
+//    $control == '#password' ? 16
+//    : 
+//    $control == '#password-confirm' ? 16
+//    :
+//    0;
+//    // console.log(output);
+//    return output;
+// };
 
-/////////////////////////  tests   //////////////////////////////
+// /////////////////////////  tests   //////////////////////////////
 
-// TEST the maxLength for this control ONLOAD
-function checkMaxLength(checkControl){
+// // TEST the maxLength for this control ONLOAD
+// function checkMaxLength(checkControl){
 
-	// console.log(checkControl + ' before ');
-   var output = 
-   checkControl == '#first-name' ? 32
-   : 
-   checkControl == '#about-yourself' ? 140
-   : 
-   checkControl == '#password' ? 16
-   : 
-   checkControl == '#password-confirm' ? 16
-   :
-   0;
-   // console.log(checkControl + ' after ');
-   // console.log(output);
-   return output;
-};
+// 	// console.log(checkControl + ' before ');
+//    var output = 
+//    checkControl == '#first-name' ? 32
+//    : 
+//    checkControl == '#about-yourself' ? 140
+//    : 
+//    checkControl == '#password' ? 16
+//    : 
+//    checkControl == '#password-confirm' ? 16
+//    :
+//    0;
+//    // console.log(checkControl + ' after ');
+//    // console.log(output);
+//    return output;
+// };
 
 // checkMaxLength('#about-yourself');
 
