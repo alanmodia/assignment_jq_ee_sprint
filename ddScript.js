2fcd1058924a9bc1234c1173ebf98de83f1f6f40
alanmodia <github@alanmillar.com>
2017-07-21 16:02:33 -0700

    completed dropdown, started phototagging

--- ddScript.js
+++ ddScript.js
@@ -1,4 +1,13 @@
-// ===== button click validation =====
+// ===== on anchor click change value of div  =====
 $( "a" ).click(function () {
-  console.log(this);
+	var value = $(this).text();
+	$( 'div.selected-value' ).text(value);
+	$('#dd').slideToggle( "slow", function() {
+	});
+});
+
+// ===== toggle visibility and slide out dropdown items =====
+$( 'div.selected-value' ).on('click', function( event ) {
+	$('#dd').slideToggle( "slow", function() {
+	});
 });
