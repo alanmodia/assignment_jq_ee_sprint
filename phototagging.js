5924dc3779f1469ce109425a8126db6ec1c0c792
alanmodia <github@alanmillar.com>
2017-07-27 19:28:41 -0700

    Fixed pageLoad CSS with "absolute" instead of "fixed" positioning; fixed js photoClick function by setting newDiv styles to click x and y and display to "block"

--- phototagging.js
+++ phototagging.js
@@ -29,25 +29,44 @@ function photoBox(e) {
 $(".photo").on("mousemove", photoBox);
 // photo.addEventListener('mousemove', photoBox);
 
+
 function photoClick(c) {
-	var newDiv = document.createElement('div');
-	newDiv.classList.add('photobox');
-	var newText = document.createTextNode("Blessed Acolytes");
-	newDiv.appendChild(newText);
+  var newDiv = document.createElement('div');
+  newDiv.classList.add('photobox');
 
-	var currentDiv = document.getElementById("acolyte");
-	document.body.insertBefore(newDiv, currentDiv);
+  var currentDiv = document.getElementById("acolyte");
+  document.body.insertBefore(newDiv, currentDiv);
 
-	// const { pageX: x, pageY: y } = c;
-	// x = x - 75;
-	// y = y - 75;
+  let { pageX: x, pageY: y } = c;
+  x = x - 75;
+  y = y - 75;
 
-	console.log(this);
-	console.log(c);
-	console.log(newDiv);
-
+  newDiv.style.left = `${ x }px`;
+  newDiv.style.top = `${ y }px`;
+  newDiv.style.display = 'block';
 }
 photo.addEventListener('click', photoClick);
+
+// badRobot
+// function photoClick(c) {
+// 	var newDiv = document.createElement('div');
+// 	newDiv.classList.add('newPhotobox');
+// 	var newText = document.createTextNode("Blessed Acolytes");
+// 	newDiv.appendChild(newText);
+
+// 	var currentDiv = document.getElementById("acolyte");
+// 	document.body.insertBefore(newDiv, currentDiv);
+
+// 	// const { pageX: x, pageY: y } = c;
+// 	// x = x - 75;
+// 	// y = y - 75;
+
+// 	console.log(this);
+// 	console.log(c);
+// 	console.log(newDiv);
+
+// }
+// photo.addEventListener('click', photoClick);
 
 
 // $( '.photo' ).on( "mousemove", function( event ) {
